a4ecf24f04d37c8b91ca99d5bc870d32a1dd2e95
Nabil Hayoun <nhayoun@c1r2p7.1337.ma>
2025-03-09 23:51:41 +0000

    Commit

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <stack>
+
+/*
+    - std::stack container - is not iterable
+    - goal to make std::stack iterable
+    Write MutantStack class , implemented in terms of a std::stacl
+    will offer all its member fucntions plus an addiotaional feature iterators
+    
+*/
+
+#include "MutantStack.tpp"
---
+++ cpp08/ex02/MutantStack.tpp
@@ -0,0 +1,13 @@
+#include "MutantStack.hpp"
+
+template <typename T>
+class MutantStack : public std::stack<T> {
+    public:
+        MutantStack() : std::stack<T>() {};
+        
+
+    
+
+};
+
+// this->c[0];
